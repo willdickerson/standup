687e408e964b31c98cd285d06c178de9884a4cae
willdickerson <will.e.dickerson@gmail.com>
2023-04-23 11:58:59 -0500

    refactor database methods and move discussion topic to session object

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import random
 import openai
 import os
-from database import create_table, get_database_connection
+from database import create_table, get_team_members, add_member, delete_member
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('FLASK_SECRET_KEY')
 create_table()
 
 DISCUSSION_SEEDS = [
@@ -59,61 +60,33 @@ DISCUSSION_SEEDS = [
     "fitness"
 ]
 
-def get_team_members():
-    conn = get_database_connection()
-    cur = conn.cursor()
-    cur.execute('SELECT name FROM team_members')
-    team_members = [row[0] for row in cur.fetchall()]
-    cur.close()
-    conn.close()
-    return team_members
-
-def delete_member():
-    member_to_delete = request.form['member_to_delete']
-    conn = get_database_connection()
-    cur = conn.cursor()
-    cur.execute('DELETE FROM team_members WHERE name = %s', (member_to_delete,))
-    conn.commit()
-    cur.close()
-    conn.close()
-    return '', 204
-
-def add_member():
-    new_member = request.form['new_member']
-    conn = get_database_connection()
-    cur = conn.cursor()
-    cur.execute('INSERT INTO team_members (name) VALUES (%s)', (new_member,))
-    conn.commit()
-    cur.close()
-    conn.close()
-    return '', 204
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+OPENAI_MODEL = "gpt-3.5-turbo"
 
 def get_random_discussion_topic():
     seed = random.choice(DISCUSSION_SEEDS)
-    openai.api_key = os.environ.get('OPENAI_API_KEY')
+    openai.api_key = OPENAI_API_KEY
     prompt = f"Hey ChatGPT, can you give us a random fun question to discuss during our standup tomorrow about {seed}? Something that everyone can answer quickly and easily that will make us laugh and smile. Please provide only the discussion question and no other text."
-    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}])
+    completion = openai.ChatCompletion.create(model=OPENAI_MODEL, messages=[{"role": "user", "content": prompt}])
     return completion.choices[0].message.content
-
-discussion_topic = ""
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    global discussion_topic
-
     if request.method == 'POST':
         action_type = request.form.get('action_type')
 
         if action_type == 'add_member':
-            add_member()
+            add_member(request)
         elif action_type == 'delete_member':
-            delete_member()
+            delete_member(request)
 
     team_members = get_team_members()
 
     if request.form.get('generate_new_order_and_topic') == 'true':
         random.shuffle(team_members)
-        discussion_topic = get_random_discussion_topic()
+        session['discussion_topic'] = get_random_discussion_topic()
+
+    discussion_topic = session.get('discussion_topic', "")
 
     return render_template('index.html', team_members=team_members, discussion_topic=discussion_topic)
 
--- database.py
+++ database.py
@@ -5,10 +5,29 @@ def get_database_connection():
     return psycopg2.connect(os.environ.get('DATABASE_URL'))
 
 def create_table():
-    conn = get_database_connection()
-    cur = conn.cursor()
-    cur.execute('''CREATE TABLE IF NOT EXISTS team_members
-                   (id SERIAL PRIMARY KEY, name TEXT UNIQUE)''')
-    conn.commit()
-    cur.close()
-    conn.close()
+    with get_database_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute('''CREATE TABLE IF NOT EXISTS team_members
+                           (id SERIAL PRIMARY KEY, name TEXT UNIQUE)''')
+            conn.commit()
+
+def get_team_members():
+    with get_database_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute('SELECT name FROM team_members')
+            team_members = [row[0] for row in cur.fetchall()]
+    return team_members
+
+def delete_member(request):
+    member_to_delete = request.form['member_to_delete']
+    with get_database_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute('DELETE FROM team_members WHERE name = %s', (member_to_delete,))
+            conn.commit()
+
+def add_member(request):
+    new_member = request.form['new_member']
+    with get_database_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute('INSERT INTO team_members (name) VALUES (%s)', (new_member,))
+            conn.commit()
