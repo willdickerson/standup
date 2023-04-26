642987287752975566086f7ce37d20428f3a56a1
willdickerson <will.e.dickerson@gmail.com>
2023-04-25 23:40:12 -0500

    generate three discussion questions and update seeds

--- app.py
+++ app.py
@@ -9,62 +9,54 @@ app.secret_key = os.environ.get('FLASK_SECRET_KEY')
 create_table()
 
 DISCUSSION_SEEDS = [
+    "kubernetes",
+    "docker",
+    "github actions",
+    "github",
+    "chatgpt",
     "food",
     "music",
     "travel",
     "technology",
-    "sports",
     "movies",
     "TV shows",
     "books",
     "video games",
-    "board games",
-    "cartoons",
-    "memes",
     "pets",
     "languages",
-    "sarcasm",
-    "jokes",
-    "puns",
-    "celebrities",
-    "myths",
-    "legends",
     "superheroes",
-    "villains",
     "robots",
     "aliens",
     "time travel",
     "magic",
     "fantasy",
     "sci-fi",
-    "horror",
     "comedy",
-    "drama",
     "action",
     "adventure",
     "mystery",
     "history",
-    "politics",
     "philosophy",
-    "religion",
     "art",
     "design",
     "fashion",
     "architecture",
     "nature",
     "environment",
-    "education",
-    "career",
-    "money",
     "health",
-    "fitness"
+    "freight logistics",
+    "kotlin",
+    "javascript",
+    "typescript",
+    "react",
+    "software engineering",
+    "quality assurance"
 ]
 
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 OPENAI_MODEL = "gpt-3.5-turbo"
 
-def get_random_discussion_topic():
-    seed = random.choice(DISCUSSION_SEEDS)
+def get_random_discussion_topic(seed):
     openai.api_key = OPENAI_API_KEY
     prompt = f"Hey ChatGPT, can you give us a random fun question to discuss during our standup tomorrow about {seed}? Something that everyone can answer quickly and easily that will make us laugh and smile. Please provide only the discussion question and no other text."
     completion = openai.ChatCompletion.create(model=OPENAI_MODEL, messages=[{"role": "user", "content": prompt}])
@@ -84,11 +76,15 @@ def index():
 
     if request.form.get('generate_new_order_and_topic') == 'true':
         random.shuffle(team_members)
-        session['discussion_topic'] = get_random_discussion_topic()
+        session['discussion_topics'] = [
+            get_random_discussion_topic(random.choice(DISCUSSION_SEEDS)),
+            get_random_discussion_topic(random.choice(DISCUSSION_SEEDS)),
+            get_random_discussion_topic(random.choice(DISCUSSION_SEEDS)),
+        ]
 
-    discussion_topic = session.get('discussion_topic', "")
+    discussion_topics = session.get('discussion_topics', [])
 
-    return render_template('index.html', team_members=team_members, discussion_topic=discussion_topic)
+    return render_template('index.html', team_members=team_members, discussion_topics=discussion_topics)
 
 if __name__ == '__main__':
     app.run(debug=True)
